cd3616a969f21b9277a7707284a77676ec63edd9
bounce <johan@waytogo.se>
2020-03-18 17:05:50 +0100

    removed testing from this branch

--- cypress/integration/userCanSeeFirstHeader.feature.js
+++
@@ -1,9 +0,0 @@
-describe("user views menus", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3001");
-  });
-
-  it("displays first header header", () => {
-    cy.get("h1").should("contain", "VAMOS TEAM 2");
-  });
-});

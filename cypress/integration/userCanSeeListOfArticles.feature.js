8785a29775f94141ea9496ab5a34c6ae7c672425
Blake <blake@futchi.com>
2020-03-19 17:27:29 +0100

    attempting axios stub for fixture file on local host

--- cypress/integration/userCanSeeListOfArticles.feature.js
+++ cypress/integration/userCanSeeListOfArticles.feature.js
@@ -1,10 +1,16 @@
 describe("user views menus", () => {
   beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/articles",
+      response: "fixture:articles_index.json"
+    });
     cy.visit("/");
   });
 
   it("views list of articles", () => {
-    cy.get(".article-headline").should("contain", "Article Headline")
-    cy.get(".article-teaser").should("contain", "Article teaser...")
+    cy.get(".article-headline").should("contain", "Coronavirus")
+    cy.get(".article-teaser").should("contain", "The World's governments cannot manage it")
   });
 });
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from "react"
 import { Box, Grommet } from 'grommet';
 import { grommet } from "grommet/themes";
+import axios from "axios";
+
+
 class ArticleList extends Component {
+  state={
+    articleIndex: []
+  };
   
+  componentDidMount() {
+    axios.get("/articles").then(response => {
+      this.setState({
+      articleIndex: response.data.articles
+      });
+      debugger
+    });
+  } 
   render() { 
+    let articleDisplay;
+    if (this.state.articleIndex !== []) {
+      articleDisplay = this.state.articleIndex.map(articles => {
+        return (
+          <>
+            {articles.headline} {articles.teaser}
+          </>
+        );
+        debugger
+      })
+    }
     return ( 
       <>
       <Grommet full theme={grommet}>
@@ -13,54 +38,14 @@ class ArticleList extends Component {
           pad="medium"
           margin="medium">
           <div className="article-headline">
-            <h2>Article Headline</h2>
+            <h2>yo</h2>
         
           <div className="article-teaser">
-            <p>Article teaser...</p>
-          </div>
-          </div>
-        </Box>
-        <Box
-          direction="row"
-          border={{ color: 'brand', size: 'large' }}
-          pad="medium"
-          margin="medium">
-          <div className="article-headline">
-            <h2>Article Headline</h2>
-        
-          <div className="article-teaser">
-            <p>Article teaser...</p>
-          </div>
-          </div>
-        </Box>
-        <Box
-          direction="row"
-          border={{ color: 'brand', size: 'large' }}
-          pad="medium"
-          margin="medium">
-          <div className="article-headline">
-            <h2>Article Headline</h2>
-        
-          <div className="article-teaser">
-            <p>Article teaser...</p>
-          </div>
-          </div>
-        </Box>
-        <Box
-          direction="row"
-          border={{ color: 'brand', size: 'large' }}
-          pad="medium"
-          margin="medium">
-          <div className="article-headline">
-            <h2>Article Headline</h2>
-        
-          <div className="article-teaser">
-            <p>Article teaser...</p>
+            <p>yo</p>
           </div>
           </div>
         </Box>
       </Grommet>
-
       </>
      );
   }

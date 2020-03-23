e8652925ce0b775b40d60eccfec6cef71a543778
bounce <johan@waytogo.se>
2020-03-23 09:51:59 +0100

    fixed indentation

--- src/state/actions/articleActions.js
+++ src/state/actions/articleActions.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const apiURL = "http://localhost:3000/api/articles";
 
 const fetchArticles = () => {
-	return async dispatch => {
-		let response = await axios.get(apiURL)
-	return dispatch(dispatchArticleAction(response.data))
-	}
-}
+  return async dispatch => {
+    let response = await axios.get(apiURL);
+    return dispatch(dispatchArticleAction(response.data));
+  };
+};
 
 const dispatchArticleAction = json => {
-	return { type: "ARTICLES", payload: json }
-}
+  return { type: "ARTICLES", payload: json };
+};
 
-export default { fetchArticles }
+export default { fetchArticles };

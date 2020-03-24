1f86f93cd1e0f20a72c4e230a6fc31340c255a4e
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-24 09:00:01 +0100

    changed axios url to deployed heroku site

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import configureStore from './state/store/configureStore'
-
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/api/";
+
+// axios.defaults.baseURL = "http://localhost:3000/api/";
+axios.defaults.baseURL = "https://urban-living.herokuapp.com/api/";
+
 
 const store = configureStore();
 window.store = store;

f29b5ce5379dd8ddb1df9edc953c13661b6eb470
Thomas <thomas@craftacademy.se>
2020-03-26 10:16:21 +0100

    format code in test

--- cypress/integration/userCanOnlySeeValidContent.feature.js
+++ cypress/integration/userCanOnlySeeValidContent.feature.js
@@ -1,74 +1,84 @@
 describe("regular user can only ", () => {
-	beforeEach(() => {
-		cy.server();
-		cy.route({
-			method: "GET",
-			url: "http://localhost:3000/api/articles",
-			response: "fixture:articles_index.json"
-		});
-		cy.route({
-			method: "GET",
-			url: "http://localhost:3000/api/articles/2",
-			response: "fixture:specific_article.json"
-		});
-		cy.visit("/");
-	});
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/articles",
+      response: "fixture:articles_index.json"
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/articles/2",
+      response: "fixture:specific_article.json"
+    });
+    cy.visit("/");
+  });
 
-	it("Not logged in can only see redacted article", () => {
-		cy.get("#2")
-			.last()
-			.within(() => {
-				cy.get("button")
-					.contains("Read More")
-					.click();
-			});
-		cy.get(".article")
-			.within(() => {
-				cy.get(".spec-title").should("contain", "Toilet");
-				cy.get(".spec-content").should("contain", "...");
-				cy.get('p').should("contain", 'This article require a premium membership.')
-			})
-	});
+  it("Not logged in can only see redacted article", () => {
+    cy.get("#2")
+      .last()
+      .within(() => {
+        cy.get("button")
+          .contains("Read More")
+          .click();
+      });
+    cy.get(".article")
+      .within(() => {
+        cy.get(".spec-title").should("contain", "Toilet");
+        cy.get(".spec-content").should("contain", "...");
+        cy.get('p').should("contain", 'This article require a premium membership.')
+      })
+  });
 
-	it('Authenticated user that is NOT a SUBSCRIBER can only see redacted article', () => {
-		cy.window()
-			.then(window => {
-				window.store.dispatch(
-					{ type: "AUTHENTICATE", payload: { currentUser: { email: 'thomas@craft.com', role: '' } } }
-				)
-			})
-		cy.get("#2")
-			.last()
-			.within(() => {
-				cy.get("button")
-					.contains("Read More")
-					.click();
-			});
-		cy.get(".article")
-			.within(() => {
-				cy.get(".spec-title").should("contain", "Toilet");
-				cy.get(".spec-content").should("contain", "...");
-				cy.get('p').should("contain", 'This article require a premium membership.')
-			})
-	});
+  it('Authenticated user that is NOT a SUBSCRIBER can only see redacted article', () => {
+    cy.window()
+      .then(window => {
+        window.store.dispatch(
+          {
+            type: "AUTHENTICATE",
+            payload: { currentUser: { email: 'thomas@craft.com', role: '' } }
+          }
+        )
+      })
+    cy.get("#2")
+      .last()
+      .within(() => {
+        cy.get("button")
+          .contains("Read More")
+          .click();
+      });
+    cy.get(".article")
+      .within(() => {
+        cy.get(".spec-title").should("contain", "Toilet");
+        cy.get(".spec-content").should("contain", "...");
+        cy.get('p').should("contain", 'This article require a premium membership.')
+      })
+  });
 
-	it('Authenticated user that is a SUBSCRIBER can se full length of article', () => {
-		cy.window().then(window => window.store.dispatch({ type: "AUTHENTICATE", payload: { currentUser: { email: 'thomas@craft.com', role: 'subscriber' } } }))
-		cy.get("#2")
-			.last()
-			.within(() => {
-				cy.get("button")
-					.contains("Read More")
-					.click();
-			});
-		cy.get(".article")
-			.within(() => {
-				cy.get(".spec-title").should("contain", "Toilet");
-			})
-		cy.get(".article")
-			.should("not.contain", "...")
-			.and("not.contain", 'This article require a premium membership.')
-	});
+  it('Authenticated user that is a SUBSCRIBER can se full length of article', () => {
+    cy.window()
+      .then(window => {
+        window.store.dispatch(
+          {
+            type: "AUTHENTICATE",
+            payload: { currentUser: { email: 'thomas@craft.com', role: 'subscriber' } }
+          })
+      })
+    cy.get("#2")
+      .last()
+      .within(() => {
+        cy.get("button")
+          .contains("Read More")
+          .click();
+      });
+    cy.get(".article")
+      .within(() => {
+        cy.get(".spec-title").should("contain", "Toilet");
+      })
+    cy.get(".article")
+      .should("not.contain", "...")
+      .and("not.contain", 'This article require a premium membership.')
+  });
 
 
 });

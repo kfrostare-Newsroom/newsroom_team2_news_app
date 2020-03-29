289eedbfe2b40b8109ccd86d2c7b9a01d55971e2
Hunter <hmvitous@yahoo.com>
2020-03-29 15:40:23 +0200

    Creates test for stripe payment form

--- cypress/integration/userCanPayForSubscription.feature.js
+++ cypress/integration/userCanPayForSubscription.feature.js
@@ -22,16 +22,31 @@ describe("User can pay for subscription:", () => {
   });
 
   it("click in buy subscription", () => {
-    cy.get("button").contains("Buy Subscription").click()
-    cy.wait(1000)
-    cy.get("form[id='payment-form']")
-      .should("be.visible")
-    cy.get('iframe[name^="__privateStripeFrame5"]')
-      .then($iframe => {
-        const $body = $iframe.contents().find('body');
-        cy.wrap($body)
-          .find('input[name="cardnumber"]')
-          .type('4242424242424242', { delay: 10 })
-      })
-  })
+    cy.get("button")
+      .contains("Buy Subscription")
+      .click();
+    cy.wait(1000);
+    cy.get("form[id='payment-form']").should("be.visible");
+    cy.get('iframe[name^="__privateStripeFrame5"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cardnumber"]')
+        .type("4242424242424242", { delay: 10 });
+    });
+    cy.get('iframe[name^="__privateStripeFrame6"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="exp-date"]')
+        .type("0425", { delay: 10 });
+    });
+    cy.get('iframe[name^="__privateStripeFrame7"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cvc"]')
+        .type("575", { delay: 10 });
+    });
+    cy.get('button')
+    .contains("Submit Payment")
+    .click()
+  });
 });

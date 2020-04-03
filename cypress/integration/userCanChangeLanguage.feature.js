d88ba336100743a6ee0d3353d5670b3b8e08ccf7
bounce <johan@waytogo.se>
2020-04-03 10:47:53 +0200

    fixed typo in cypress test

--- cypress/integration/userCanChangeLanguage.feature.js
+++ cypress/integration/userCanChangeLanguage.feature.js
@@ -19,7 +19,7 @@ describe("Index path:", () => {
     cy.get("#spanish").should("contain", "Español")
   });
 
-  it('changes to swedish when I tell i to', () => {
+  it('changes to swedish when I tell it to', () => {
     cy.get("#dropdown-menu")
       .should("contain", "Language")
       .click();
@@ -28,7 +28,7 @@ describe("Index path:", () => {
     cy.get("#tagline").should("contain", "Din källa för inspiration och harmoni i arbetslivet.") 
   });
 
-  it('changes to spanish when I tell i to', () => {
+  it('changes to spanish when I tell it to', () => {
     cy.get("#dropdown-menu")
       .should("contain", "Language")
       .click();
@@ -37,7 +37,7 @@ describe("Index path:", () => {
     cy.get("#tagline").should("contain", "Fuente de inspiración laboral para jóvenes profesionales.") 
   });
 
-  it('changes to english when I tell i to', () => {
+  it('changes to english when I tell it to', () => {
     cy.get("#dropdown-menu")
       .should("contain", "Language")
       .click();
